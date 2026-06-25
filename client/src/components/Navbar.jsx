e1d984d9f533eeb819416e72df62cdde45d9b93a
Bizimana Eric <ericbizimana111@gmail.com>
2026-06-25 19:29:59 +0200

    main

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = ({ setShowLogin }) => {
                 <div className='flex max-sm:flex-col items:start sm:items-center gap-6'>
 
                     <button onClick={() => navigate('/owner')}
-                        className='cursor-pointer'>Dashboard</button>
+                        className='cursor-pointer'>Dashboard</
 
                     <button onClick={() => setShowLogin(true)}
                         className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull 
@@ -51,13 +51,9 @@ const Navbar = ({ setShowLogin }) => {
 
             </div>
 
-
             <button className='sm:hidden cursor:pointer' arial-label="Menu" onClick={() => setOpen(!open)}>
                 <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
             </button>
-
-            
-            
 
         </div>
     )
