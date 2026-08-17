a84f6c9b1ce5ffc1173638391750f268994ad528
Bizimana Eric <ericbizimana111@gmail.com>
2026-08-17 12:30:00 +0200

    added codes

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from "react-router-dom";
+
 
 const CarCard = ({ car }) => {
 
-    // import.meta give acces to vite sytem info 
-    //env environment variable
-    //VITE_.. your custom varible
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
+
 
 
     return (
-        <div className='group rounded-xl overflow-hidden shadow-lg hover:translate-y-1 transition-all duration-500 cursor-pointer'>
+        <div onClick={() => { navigate(`/car-details/${car._id}`); scrollTo(0, 0) }}
+            className='group rounded-xl overflow-hidden shadow-lg hover:translate-y-1 transition-all duration-500 cursor-pointer'>
 
             <div className='relative h-48 overflow-hidden'>
                 <img src={car.image} alt="Car Image" className='w-full h-full object-cover  transition-transform duration-500 group-hover:scale-105' />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+
+
 
 const Home = () => {
   return (
     <div>
-
-
-        
+      <Hero />
+      <FeaturedSection />
+      <Banner />
     </div>
   )
 }
